eece79c6d93f5d4c46961564abbfe3ff79fb2928
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2024-02-23 10:29:48 +0100

    added kingtree sanctuary page

--- generators/FullReleaseGenerator.kt
+++ generators/FullReleaseGenerator.kt
@@ -2,21 +2,41 @@ package generators
 
 import java.io.File
 
+private enum class Destination(
+    val targetRefsRoot: String,
+    val refsImgReplacement: String,
+    val refsDir: String,
+    val refsImgDir: String
+) {
+    DOCS(
+        targetRefsRoot = "docs",
+        refsImgReplacement = "../../refs/img",
+        refsDir = "refs",
+        refsImgDir = "refs/img"
+    ),
+    ZEITHALT_REPO(
+        targetRefsRoot = "public_refs",
+        refsImgReplacement = "../i",
+        refsDir = "r",
+        refsImgDir = "i"
+    )
+}
+
 fun main() {
     println("Started full release generation!")
     val startTime = System.currentTimeMillis()
 
-    generateFullRelease()
+    generateFullRelease(Destination.ZEITHALT_REPO)
 
     val endTime = System.currentTimeMillis()
     println("Finished full release generation in ${endTime - startTime} ms")
 }
 
-private fun generateFullRelease() {
+private fun generateFullRelease(destination: Destination) {
     val linkMap = rebuildLinkMap()
 
-    copyRefs(linkMap)
-    copyRefsImg()
+    copyRefs(destination, linkMap)
+    copyRefsImg(destination)
 
     copyTimeline()
     copyTimelineMap()
@@ -25,7 +45,10 @@ private fun generateFullRelease() {
     rebuildTimelineIndex(linkMap)
 }
 
-private fun copyRefs(linkMap: Map<String, String>) {
+private fun copyRefs(
+    destination: Destination,
+    linkMap: Map<String, String>
+) {
     val refs = File("refs")
         .listFiles()
         ?.filter { it.isFile }
@@ -35,26 +58,28 @@ private fun copyRefs(linkMap: Map<String, String>) {
             val anchor = linkMap["../refs/${file.name}"] ?: ""
 
             val updatedImgLinks = text
-                .replace("../refs/img", "../../refs/img")
+                .replace("../refs/img", destination.refsImgReplacement)
                 .split("<!---")
                 .first()
                 .plus("\n")
                 .plus("----------\n")
-                .plus("[⬅️ Back to index](../refs/#${anchor}_s)")
+                .plus("[⬅️ Back to index](../${destination.refsDir}/#${anchor}_s)")
 
-            File("docs/refs/${file.name}")
+            File("${destination.targetRefsRoot}/${destination.refsDir}/${file.name}")
                 .writeText(updatedImgLinks)
         }
 
     println("Copied ${refs?.size ?: "0"} refs entries")
 }
 
-private fun copyRefsImg() {
+private fun copyRefsImg(
+    destination: Destination
+) {
     val img = File("refs/img")
         .listFiles()
         ?.filter { it.isFile }
         ?.onEach { file ->
-            file.copyTo(File("docs/refs/img/${file.name}"), overwrite = true)
+            file.copyTo(File("${destination.targetRefsRoot}/${destination.refsImgDir}/${file.name}"), overwrite = true)
         }
 
     println("Copied ${img?.size ?: "0"} refs images")
